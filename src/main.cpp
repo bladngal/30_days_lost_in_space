c483f80803aa049859ad8b946feb11c8fbb9d4a1
Laura Maher <laura@maher-family.net>
2021-01-04 18:01:35 -0800

    Three lights!

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,43 @@
 #include <Arduino.h>
 #include "blink.h"
 
-int LED = 12;
+int LED10 = 10;
+int LED11 = 11;
+int LED12 = 12;
 int Switch1 = 2;
+int Switch2 = 3;
+int Switch3 = 4;
 
 void setup() {
-    pinMode(LED, OUTPUT);
+    pinMode(LED10, OUTPUT);
     pinMode(Switch1, INPUT);  
+    pinMode(LED11, OUTPUT);
+    pinMode(Switch2, INPUT);
+    pinMode(LED12, OUTPUT);
+    pinMode(Switch3, INPUT);
 }
 
 void loop() {
+  //Switch number 1
   if(digitalRead(Switch1) == 1){
-    digitalWrite(LED, HIGH); //send 5V aka ON
+    digitalWrite(LED10, HIGH); //send 5V aka ON
   }
   else{
-    digitalWrite(LED, LOW); //turn it off aka GROUND
+    digitalWrite(LED10, LOW); //turn it off aka GROUND
+  }
+  //Switch number 2
+  if(digitalRead(Switch2) == 1){
+    digitalWrite(LED11, HIGH); //send 5V aka ON
+  }
+  else{
+    digitalWrite(LED11, LOW); //turn it off aka GROUND
+  }
+  //Switch number 3
+  if(digitalRead(Switch3) == 1){
+    digitalWrite(LED12, HIGH); //send 5V aka ON
+  }
+  else{
+    digitalWrite(LED12, LOW); //turn it off aka GROUND
   }
   
   
