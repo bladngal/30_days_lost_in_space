d51b62ffbca29f9dd05e3d832b5399451e925584
Laura Maher <laura@maher-family.net>
2021-01-05 17:54:54 -0800

    Made a tripwire!

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,28 @@
 //#define onboard 13
 
 int LED = 12;
+int tripWirePin = 8;
+int buzzerPin = 7;
 
 void setup() {
-    pinMode(LED, OUTPUT);
-    //Serial.begin(9600);
+    //pinMode(LED, OUTPUT);
+
+    Serial.begin(9600);
     
 }
 
 
 void loop() {
   
+  //system is intact and untripped
+  if(digitalRead(tripWirePin) == 1) {
+    Serial.println("System Armed");
+  }
+else {
+  tone(buzzerPin, 250, 500);
   blink(LED, 1000);
+  Serial.println("INTRUDER ALERT!!");
+  noTone(buzzerPin);
+}
     Serial.println("Loop complete");
 }
