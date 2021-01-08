e9cb06f3e7d589eb34ee1b50411bcb3c1ed78416
Laura Maher <laura@maher-family.net>
2021-01-07 18:57:45 -0800

    Playing with blue light shift

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 int red = 11;
 int green = 10;
 int blue = 9;
+int sensorPin = A0;
+int sensorValue = 0;
+int blueValue = 255;
  
 void RGB_color(int red_value, int green_value, int blue_value)
 {
@@ -12,24 +15,32 @@ void RGB_color(int red_value, int green_value, int blue_value)
   analogWrite(blue, blue_value);
 } 
 
+int getBlueLightValue(int sensorValue){
+  int blueLightValue = 255; //no blue light
+  //I'm calling 500 max on sensor value
+  int maxValue = 500;
+  double sensorPercentage = (double)sensorValue/(double)maxValue;
+  double inversePercentage = 1 - sensorPercentage;
+  blueLightValue = inversePercentage * blueLightValue;
+  return blueLightValue;
+}
+
 void setup() {
+  Serial.begin(9600);
+
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
 }
  
 void loop() {
-  RGB_color(125, 0, 0); // Red
-  delay(500);
-  RGB_color(0, 125, 0); // Green
-  delay(500);
-  RGB_color(0, 0, 125); // Blue
-  delay(500);
-  RGB_color(0, 125, 125); // yellow
-  delay(500);
-  RGB_color(125, 0, 125); // purple
-  delay(500);
-  RGB_color(125, 125, 125); // white
-  delay(500);
+  sensorValue = analogRead(sensorPin);
+
+  //more sensor value = more blue light
+
+  int blueLightValue = getBlueLightValue(sensorValue);
+  int redLightValue = 255 - blueLightValue;
+  RGB_color(redLightValue, 255,blueLightValue ); // Blue
+  delay(50);
 }
  
