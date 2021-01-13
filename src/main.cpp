1785a64218a11b5214734f9a38a4d54b6c6044d2
Laura Maher <laura@maher-family.net>
2021-01-12 16:58:16 -0800

    Playing tones

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,60 @@
-// src/main.cpp
-#include <Arduino.h>
-
-int red = 11;
-int green = 10;
-int blue = 9;
+// Code written by Alexander Eschenauer for the 30 Day Adventure Kit
+// Learn more at https://inventr.io/adventure
  
-void RGB_color(int red_value, int green_value, int blue_value)
-{
-  analogWrite(red, red_value);
-  analogWrite(green, green_value);
-  analogWrite(blue, blue_value);
-} 
+#include <Keypad.h>
+ 
+const byte ROWS = 4;
+const byte COLS = 4;
 
-void setup() {
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
+int buzzer = 10;  
+ 
+char buttons[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+ 
+// byte rowPins[ROWS] = {5, 4, 3, 2};
+// byte colPins[COLS] = {2, 3, 4, 5};
+
+byte rowPins[ROWS] = {5, 4, 3, 2};
+byte colPins[COLS] = {6, 7 ,8 ,9};
+
+int tones[ROWS][COLS] = {
+    {31, 93, 147, 208},
+    {247, 311, 370, 440},
+    {523, 587, 698, 880},
+    {1397, 2637, 3729, 4978}
+};
+ 
+// Using the Keypad.h Library, we are going to define a custom keypad.
+// In order to do that, it needs to know the following:
+// What the keys are (a 2D Array of characters (char)),
+// what pins are being used (2 thru 9),
+// how many rows there are (4), and how many columns (4).
+Keypad customKeypad = Keypad(makeKeymap(buttons), rowPins, colPins, ROWS, COLS);
+ 
+void setup(){
+  Serial.begin(9600); // Begin monitoring via the serial monitor
 }
  
 void loop() {
-  RGB_color(125, 0, 0); // Red
-  delay(500);
-  RGB_color(0, 125, 0); // Green
-  delay(500);
-  RGB_color(0, 0, 125); // Blue
-  delay(500);
-  RGB_color(0, 125, 125); // yellow
-  delay(500);
-  RGB_color(125, 0, 125); // purple
-  delay(500);
-  RGB_color(125, 125, 125); // white
-  delay(500);
+  int toneFreq = 0;
+  char customKey = customKeypad.getKey();
+  if (customKey) { //if a button is pressed
+    Serial.print(customKey);
+    Serial.print(": ");
+    for (byte j=0; j<ROWS; j++) {
+        for (byte i=0; i<COLS; i++) {
+            if (customKey == buttons[j][i]) {
+                toneFreq=tones[j][i];
+                break;
+            }
+        }
+    }
+    tone(buzzer, toneFreq, 500);
+    delay(500);
+    noTone(buzzer);
+  }
 }
- 
