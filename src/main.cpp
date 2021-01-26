08f125f18eb5de5d2a32af4e217fd1fda19e6a56
Laura Maher <laura@maher-family.net>
2021-01-25 17:56:19 -0800

    Cipher code

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,23 @@
-// Code written by Alexander Eschenauer for the 30 Day Adventure Kit
-// Learn more at https://inventr.io/adventure
 #include <Arduino.h>
 #include <TM1637Display.h>
-// Define the connections pins:
-#define CLK 6
-#define DIO 5
-// Create display object of type TM1637Display:
-TM1637Display display = TM1637Display(CLK, DIO);
-// Create array that turns all segments on:
-const uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
-// Create array that turns all segments off:
-const uint8_t blank[] = {0x00, 0x00, 0x00, 0x00};
-// You can set the individual segments per digit to spell words or create other symbols:
-const uint8_t done[] = {
-    SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,          // d
-    SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,  // O
-    SEG_C | SEG_E | SEG_G,                          // n
-    SEG_A | SEG_D | SEG_E | SEG_F | SEG_G           // E
-};
-
-const uint8_t SOS[] = {
-    SEG_A | SEG_F | SEG_G | SEG_C | SEG_D,          // S
-    SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,  // O
-    SEG_A | SEG_F | SEG_G | SEG_C | SEG_D,
-    0x00  // S
-};
-
+// Module connection pins (Digital Pins)
+#define CLK 2
+#define DIO 3
+// The amount of time (in milliseconds) between tests
+#define TEST_DELAY 2000
+TM1637Display display(CLK, DIO);
 void setup() {
-    Serial.begin(9600);  // Begin monitoring via the serial monitor
-    display.clear();
-    delay(1000);
 }
-
 void loop() {
-    // Set the brightness:
-    display.setBrightness(5);
-    // All segments on:
+    display.setBrightness(0x0f);
+    uint8_t data[] = {0x0, 0x0, 0x0, 0x0};
     display.setSegments(data);
-    delay(1000);
-    display.clear();
-    delay(1000);
-    // Show counter:
-    int i;
-    for (i = 0; i <= 10; i++) {
-        display.showNumberDec(i);
-        delay(50);
-    }
-    delay(1000);
-    display.clear();
-    delay(1000);
-    display.setSegments(done);
-    delay(1000);
-    display.clear();
-    delay(1000);
-    display.setSegments(SOS);
-    while (1)
-        ;
+    display.showNumberDec(23, false, 3, 1);
+    delay(TEST_DELAY);
+    display.setSegments(data);
+    display.showNumberDec(353, true, 3, 1);
+    delay(TEST_DELAY);
+    display.setSegments(data);
+    display.showNumberDec(1688, true, 4, 0);
+    delay(TEST_DELAY);
 }
